92079fdd23954980f75a4470af0d17e2da515511
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-18 10:15:18 -0600

    Fix errors in average.js and reverse.js

--- average.js
+++ average.js
@@ -2,9 +2,10 @@ function average(list) {
     var sum = 0;
   
     for (var num of list) {
-      sum ++ num;
+      sum += num; // fixed the operators
     }
   
     return sum / list.length;
+  } //closed the function's curly brackets
   
 console.log(average([3, 5, 7]));
